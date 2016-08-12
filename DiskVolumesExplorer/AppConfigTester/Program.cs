bdc516514a8231cf469c2477babe7a8fa4172c5e
novokrest <novokrest013@gmail.com>
2016-08-12 23:01:49 +0300

    add garbage

---
+++ DiskVolumesExplorer/AppConfigTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppConfigTester.VmWare;
+
+namespace AppConfigTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = VmWareServiceConfigLoader.LoadConfig();
+            new object();
+        }
+    }
+}
---
+++ DiskVolumesExplorer/AppConfigTester/VmWare/VmWareServiceConfigSection.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+
+namespace AppConfigTester.VmWare
+{
+    public class VmWareServiceConfigSection : ConfigurationSection
+    {
+        public const string SectionName = "VmWareService";
+
+        private const string UrlProperty = "Url";
+        private const string UserProperty = "User";
+        private const string PasswordProperty = "Password";
+
+        [ConfigurationProperty(UrlProperty, DefaultValue = "", IsRequired = true)]
+        public string Url => (string) this[UrlProperty];
+
+        [ConfigurationProperty(UserProperty, DefaultValue = "", IsRequired = true)]
+        public string User => (string) this[UserProperty];
+
+        [ConfigurationProperty(PasswordProperty, DefaultValue = "", IsRequired = true)]
+        public string Password => (string) this[PasswordProperty];
+    }
+
+    public static class VmWareServiceConfigLoader
+    {
+        public static VmWareServiceConfigSection LoadConfig()
+        {
+            return (VmWareServiceConfigSection) ConfigurationManager.GetSection(VmWareServiceConfigSection.SectionName);
+        }
+    }
+}
